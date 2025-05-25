03dc3b3f261586e5644e966443c891357f318b88
lizziegitau <lizziegitau0505@gmail.com>
2025-05-25 09:55:44 +0300

    url change

--- server.js
+++ server.js
@@ -7,10 +7,20 @@ const app = express();
 dotenv.config();
 
 const port = process.env.PORT || 5000;
-const frontend_url = process.env.FRONTEND_URL
 
 app.use(bodyParser.json());
-app.use(cors({ origin: `${frontend_url}`, credentials: true }));
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 const taskRoutes = require('./routes/tasks');
 const deckRoutes = require('./routes/decks');
