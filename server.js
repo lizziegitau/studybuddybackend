654975613945bcdcdc41ff4b77536277ec1a08b7
lizziegitau <lizziegitau0505@gmail.com>
2025-05-25 11:35:47 +0300

    cors fix

--- server.js
+++ server.js
@@ -9,17 +9,15 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true
+  origin: [
+    'http://localhost:3000',
+    process.env.FRONTEND_URL
+  ].filter(Boolean),
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 const taskRoutes = require('./routes/tasks');
